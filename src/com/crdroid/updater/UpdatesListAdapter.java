9066ea5b7f1381b4127ffe426cff6969c6ec8c26
Ali B <abittin@gmail.com>
2019-03-14 18:41:08 +0300

    Updater: enable gapps-twrp flashing
    If any gapps or twrp flashable zip file is locally available,
    user can use "Choose file..." from 3-dot menu and select
    local file as an update. Updater will verify the file, and
    if good, will let the user to install it.
    
    Change-Id: I31024f013549cfe365b3de55f519d410198cbd86
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/updater/UpdatesListAdapter.java
+++ src/com/crdroid/updater/UpdatesListAdapter.java
@@ -74,6 +74,8 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
     private static final int BATTERY_PLUGGED_ANY = BatteryManager.BATTERY_PLUGGED_AC
             | BatteryManager.BATTERY_PLUGGED_USB
             | BatteryManager.BATTERY_PLUGGED_WIRELESS;
+    private static final String GAPPS_SUBSTRING = "gapps";
+    private static final String TWRP_SUBSTRING = "twrp";
 
     private final float mAlphaDisabledValue;
 
@@ -82,6 +84,7 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
     private UpdaterController mUpdaterController;
     private UpdatesListActivity mActivity;
     private UpdatesActivity mUpdatesActivity;
+    private boolean mIsGappsOrTWRP = false;
 
     private AlertDialog infoDialog;
 
@@ -224,9 +227,21 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
         } else if (update.getPersistentStatus() == UpdateStatus.Persistent.VERIFIED) {
             viewHolder.itemView.setOnLongClickListener(
                     getLongClickListener(update, true, viewHolder.mBuildDate));
-            setButtonAction(viewHolder.mAction,
-                    Utils.canInstall(update) ? Action.INSTALL : Action.DELETE,
-                    downloadId, !isBusy());
+
+            if (Utils.canInstall(update)) {
+                setButtonAction(viewHolder.mAction, Action.INSTALL,
+                        downloadId, !isBusy());
+            } else {
+                String filename = update.getName();
+                if (filename.indexOf(GAPPS_SUBSTRING) != -1 || filename.indexOf(TWRP_SUBSTRING) != -1) {
+                    setButtonAction(viewHolder.mAction, Action.INSTALL,
+                            downloadId, !isBusy());
+                    mIsGappsOrTWRP = true;
+                } else {
+                    setButtonAction(viewHolder.mAction, Action.DELETE,
+                            downloadId, !isBusy());
+                }
+            }
         } else if (!Utils.canInstall(update)) {
             viewHolder.itemView.setOnLongClickListener(
                     getLongClickListener(update, false, viewHolder.mBuildDate));
@@ -390,7 +405,7 @@ public class UpdatesListAdapter extends RecyclerView.Adapter<UpdatesListAdapter.
                 UpdateInfo update = mUpdaterController.getUpdate(downloadId);
                 final boolean canInstall = Utils.canInstall(update);
                 clickListener = enabled ? view -> {
-                    if (canInstall) {
+                    if (canInstall || mIsGappsOrTWRP) {
                         getInstallDialog(downloadId).show();
                     } else {
                         mActivity.showSnackbar(R.string.snack_update_not_installable,
