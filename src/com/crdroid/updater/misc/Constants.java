884a123652e2ad56fe7c4ae75271cfa72e1a07bc
Pranav Vashi <neobuddy89@gmail.com>
2019-10-02 00:35:01 +0530

    Updater: Update constants
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/updater/misc/Constants.java
+++ src/com/crdroid/updater/misc/Constants.java
@@ -39,10 +39,10 @@ public final class Constants {
 
     public static final String PROP_AB_DEVICE = "ro.build.ab_update";
     public static final String PROP_BUILD_DATE = "ro.build.date.utc";
-    public static final String PROP_BUILD_VERSION = "ro.crdroid.build.version";
+    public static final String PROP_BUILD_VERSION = "ro.modversion";
     public static final String PROP_DEVICE = "ro.crdroid.device";
     public static final String PROP_NEXT_DEVICE = "ro.updater.next_device";
-    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "lineage.updater.allow_downgrading";
+    public static final String PROP_UPDATER_ALLOW_DOWNGRADING = "crdroid.updater.allow_downgrading";
 
     public static final String PREF_INSTALL_OLD_TIMESTAMP = "install_old_timestamp";
     public static final String PREF_INSTALL_NEW_TIMESTAMP = "install_new_timestamp";
