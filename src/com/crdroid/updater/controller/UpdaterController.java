9470cb03abea88b9adf4adc2e7b9db3527207ddf
Dan Pasanen <dan.pasanen@gmail.com>
2019-10-30 12:43:40 -0500

    Updater: actually fire the notification on completion of A/B update
    Change-Id: I70a211b894fdcfa6a0a58f0f5a6efd911196f311

--- src/com/crdroid/updater/controller/UpdaterController.java
+++ src/com/crdroid/updater/controller/UpdaterController.java
@@ -63,6 +63,7 @@ import com.google.android.material.snackbar.Snackbar;
 public class UpdaterController {
 
     public static final String ACTION_DOWNLOAD_PROGRESS = "action_download_progress";
+    public static final String ACTION_INSTALL_FINISHED = "action_install_finished";
     public static final String ACTION_INSTALL_PROGRESS = "action_install_progress";
     public static final String ACTION_UPDATE_REMOVED = "action_update_removed";
     public static final String ACTION_UPDATE_STATUS = "action_update_status_change";
@@ -161,6 +162,13 @@ public class UpdaterController {
     void notifyInstallProgress(String downloadId) {
         Intent intent = new Intent();
         intent.setAction(ACTION_INSTALL_PROGRESS);
+        intent.putExtra(EXTRA_DOWNLOAD_ID, downloadId);
+        mBroadcastManager.sendBroadcast(intent);
+    }
+
+    void notifyInstallFinished(String downloadId) {
+        Intent intent = new Intent();
+        intent.setAction(ACTION_INSTALL_FINISHED);
         intent.putExtra(EXTRA_DOWNLOAD_ID, downloadId);
         mBroadcastManager.sendBroadcast(intent);
     }
