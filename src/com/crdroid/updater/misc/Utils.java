95e8825e31a5bed3c5c6665d6f65c2a41977fbc5
Bruno Martins <bgcngm@gmail.com>
2020-11-11 22:39:07 +0000

    Updater: Add setting for updating recovery
    When enabled, the recovery of the device will be updated with the
    version of the installed system. This is only applicable to devices
    with a dedicated recovery partition and not A/B.
    
    Change-Id: I4c1701b09c000b4cdb044069fa9ba857500f9cb1
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/updater/misc/Utils.java
+++ src/com/crdroid/updater/misc/Utils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2017-2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -446,5 +446,9 @@ public class Utils {
 
     public static String getPaypal() {
         return mPaypal;
+    }
+
+    public static boolean isRecoveryUpdateExecPresent() {
+        return new File(Constants.UPDATE_RECOVERY_EXEC).exists();
     }
 }
