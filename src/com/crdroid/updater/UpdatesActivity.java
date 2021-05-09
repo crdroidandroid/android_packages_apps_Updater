spkal01/packages_apps_Updater

Updater: add permissions check for local install
Change-Id: I8872bef72009aee91ce258d19d48e48c428eca54

src/org/lineageos/updater/UpdatesActivity.java
@@ -15,6 +15,7 @@
  */
 package org.lineageos.updater;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -60,6 +61,7 @@ import org.lineageos.updater.download.DownloadClient;
 import org.lineageos.updater.misc.BuildInfoUtils;
 import org.lineageos.updater.misc.Constants;
 import org.lineageos.updater.misc.FileUtils;
+import org.lineageos.updater.misc.PermissionsUtils;
 import org.lineageos.updater.misc.StringGenerator;
 import org.lineageos.updater.misc.Utils;
 import org.lineageos.updater.model.Update;
@@ -92,9 +94,17 @@ public class UpdatesActivity extends UpdatesListActivity {
 
     private static final int READ_REQUEST_CODE = 42;
 
+    // Storage Permissions
+    private static final int STORAGE_PERMISSIONS_REQUEST_CODE = 0;
+    private static final String[] REQUIRED_STORAGE_PERMISSIONS = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_updates);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -217,7 +227,12 @@ public class UpdatesActivity extends UpdatesListActivity {
                 return true;
             }
             case R.id.menu_local_update: {
-                performFileSearch();
+                boolean hasPermission = PermissionsUtils.checkAndRequestPermissions(
+                      this, REQUIRED_STORAGE_PERMISSIONS,
+                      STORAGE_PERMISSIONS_REQUEST_CODE);
+                if (hasPermission) {
+                  performFileSearch();
+                }
             }
         }
         return super.onOptionsItemSelected(item);

crdroidandroid/android_packages_apps_Updater

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/updater/UpdatesActivity.java
@@ -70,6 +70,7 @@ import com.crdroid.updater.download.DownloadClient;
 import com.crdroid.updater.misc.BuildInfoUtils;
 import com.crdroid.updater.misc.Constants;
 import com.crdroid.updater.misc.FileUtils;
+import com.crdroid.updater.misc.PermissionsUtils;
 import com.crdroid.updater.misc.StringGenerator;
 import com.crdroid.updater.misc.Utils;
 import com.crdroid.updater.model.Update;
@@ -101,9 +102,17 @@ public class UpdatesActivity extends UpdatesListActivity {
 
     private static Map<String, String> sf_mirrors;
 
+    // Storage Permissions
+    private static final int STORAGE_PERMISSIONS_REQUEST_CODE = 0;
+    private static final String[] REQUIRED_STORAGE_PERMISSIONS = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_updates);
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
@@ -383,7 +392,12 @@ public class UpdatesActivity extends UpdatesListActivity {
                 return true;
             }
             case R.id.menu_local_update: {
-                performFileSearch();
+                boolean hasPermission = PermissionsUtils.checkAndRequestPermissions(
+                      this, REQUIRED_STORAGE_PERMISSIONS,
+                      STORAGE_PERMISSIONS_REQUEST_CODE);
+                if (hasPermission) {
+                  performFileSearch();
+                }
                 return true;
             }
         }