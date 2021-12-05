d3dececf7896438b86f2e27e898bf79b1a69365a
Pranav Vashi <neobuddy89@gmail.com>
2021-12-05 12:17:49 +0530

    Updater: Change package name to avoid conflict
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.updater"
+        applicationId = "com.crdroid.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
