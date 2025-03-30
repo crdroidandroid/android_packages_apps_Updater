8f65bc32d6274a49dafc8dab431f63aad2e9196a
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-31 06:19:52 +0800

    Updater: Remove cryptographic signature verification for local updates
    The update will still be verified by the installer once the device is rebooted into the recovery system.
    
    Change-Id: I9bf8270611a9b9dde8effc9186d31147fdc0fb17
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/org/lineageos/updater/UpdateImporter.java
+++ app/src/main/java/org/lineageos/updater/UpdateImporter.java
@@ -76,7 +76,6 @@ public class UpdateImporter {
             File importedFile = null;
             try {
                 importedFile = importFile(uri);
-                verifyPackage(importedFile);
 
                 final Update update = buildLocalUpdate(importedFile);
                 addUpdate(update);
@@ -139,20 +138,6 @@ public class UpdateImporter {
             .setStatus(UpdateStatus.VERIFIED)
             .setVersion(name)
             .build();
-    }
-
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    private void verifyPackage(File file) throws Exception {
-        try {
-            android.os.RecoverySystem.verifyPackage(file, null, null);
-        } catch (Exception e) {
-            if (file.exists()) {
-                file.delete();
-                throw new Exception("Verification failed, file has been deleted");
-            } else {
-                throw e;
-            }
-        }
     }
 
     private void addUpdate(Update update) {
