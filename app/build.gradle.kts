6e2ad8fbbec93e19a325ebb1f0f683956eeb1324
Bobby <306bobby-android@users.noreply.github.com>
2026-05-04 14:26:28 +0000

    Updater: add crdroid logo to header

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,12 @@ val keystoreProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "org.lineageos.updater"
         minSdk = 32
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
