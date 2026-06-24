455b86145d1b3724cce78eb2be6a3e945aee396e
Pranav Vashi <neobuddy89@gmail.com>
2026-06-24 23:59:50 +0530

    Updater: Adapt colors in lottie animations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/org/lineageos/updater/ui/LottieColorUtils.kt
+++ app/src/main/java/org/lineageos/updater/ui/LottieColorUtils.kt
@@ -1,6 +1,7 @@
 /*
  * SPDX-FileCopyrightText: The Android Open Source Project
  * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-FileCopyrightText: crDroid Android Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -12,10 +13,12 @@ import android.graphics.PorterDuffColorFilter
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
 import com.airbnb.lottie.LottieProperty
 import com.airbnb.lottie.compose.LottieDynamicProperty
 import com.airbnb.lottie.compose.rememberLottieDynamicProperties
 import com.airbnb.lottie.compose.rememberLottieDynamicProperty
+import org.lineageos.updater.R
 
 internal object LottieColorUtils {
     @Composable
@@ -30,6 +33,20 @@ internal object LottieColorUtils {
     }
 
     @Composable
+    private fun createBrandFilter(): LottieDynamicProperty<ColorFilter> {
+        val color = colorResource(R.color.brand_primary)
+        return rememberLottieDynamicProperty(
+            property = LottieProperty.COLOR_FILTER,
+            keyPath = arrayOf("**", "fill", "**"),
+        ) {
+            PorterDuffColorFilter(color.toArgb(), PorterDuff.Mode.SRC_ATOP)
+        }
+    }
+
+    @Composable
     fun getDefaultDynamicProperties() =
-        rememberLottieDynamicProperties(createOnSurfaceFilter())
+        rememberLottieDynamicProperties(
+            createOnSurfaceFilter(),
+            createBrandFilter(),
+        )
 }
